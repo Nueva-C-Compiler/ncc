ba594f13659d1f596511a259c2ee30eba78de520
David Freifeld <freifeld.david@gmail.com>
2022-03-13 21:25:48 -0700

    Switch to double buffer scheme and add primitive lookahead

--- src/utils/unicode.rs
+++ src/utils/unicode.rs
@@ -1,4 +1,5 @@
-use std::fs::{File};
+use std::fs::File;
+use std::path::Path;
 use std::io::Read;
 
 /// Returns the initial codepoint accumulator for the first byte.
@@ -41,47 +42,113 @@ enum CharAtom {
 
 struct CodepointReader {
     f: File,
-    buf: [u8; 4] // TODO: Dubious
+    buf1: [u8; 4096],
+    buf2: [u8; 4096],
+    index: usize,
+    partial: Option<usize>,
+    secondary: bool,
+    last: bool
+}
+
+impl CodepointReader {
+    fn new(path: &Path) -> Self {
+        let mut reader = CodepointReader {
+            f: File::open(path).unwrap(),
+            buf1: [0; 4096],
+            buf2: [0; 4096],
+            index: 0,
+            partial: None,
+            secondary: false,
+            last: false,
+        };
+        reader.f.read(&mut reader.buf1).unwrap();
+        reader.f.read(&mut reader.buf2).unwrap();
+        reader
+    }
+
+    fn get_byte(&mut self) -> Option<u8> {
+        if self.index == 4096 {
+            if let Some(_) = self.partial {
+                self.last = true;
+            } else {
+                let sz = match self.secondary {
+                    false => self.f.read(&mut self.buf1).unwrap(),
+                    true => self.f.read(&mut self.buf2).unwrap(),
+                };
+                if sz != 4096 { self.partial = Some(sz) }
+            }
+            self.secondary = !self.secondary;
+            self.index = 0;
+        }
+
+        if self.last && self.index > self.partial.unwrap_or(4096) {
+            None
+        } else if self.secondary {
+            Some(self.buf2[self.index])
+        } else {
+            Some(self.buf1[self.index])
+        }
+    }
+
+    // TODO expand capabilities to support rest of multi code point sequences
+    fn lookahead(&self) -> Option<u8> {
+        if self.index == 4096 {
+            match self.secondary {
+                true => Some(self.buf1[0]),
+                false => Some(self.buf2[0]),
+            }
+        } else {
+            if self.index > self.partial.unwrap_or(4096) && self.last {
+                return None;
+            }
+            match self.secondary {
+                false => Some(self.buf1[self.index+1]),
+                true => Some(self.buf2[self.index+1]),
+            }
+        }
+    }
 }
 
 impl Iterator for CodepointReader {
     type Item = Codepoint;
 
-    // Basically lifted from std::core::str.
     fn next(&mut self) -> Option<Self::Item> {
-        let sz = self.f.read(&mut self.buf[..1]).unwrap();
-        if sz == 0 { return None; }
-        else if self.buf[0] < 128 {
-            return Some(Codepoint::Valid(self.buf[0] as u32));
-        }
+        let x = match self.get_byte() {
+            None => return None,
+            Some(c) => c,
+        };
+        if x < 128 { return Some(Codepoint::Valid(x as u32)); }
 
-        let init = utf8_first_byte(self.buf[0], 2);
-        let sz = self.f.read(&mut self.buf[1..2]).unwrap();
-        if sz == 0 { return Some(Codepoint::Invalid((self.buf[0] as u32) << 24)); }
-        let mut ch = utf8_acc_cont_byte(init, self.buf[1]);
-
-        if self.buf[0] >= 0xE0 {
-            let sz = self.f.read(&mut self.buf[2..3]).unwrap();
-            if sz == 0 {
-                return Some(Codepoint::Invalid(
-                    ((self.buf[0] as u32) << 24) | ((self.buf[1] as u32) << 16)
-                ))
-            }
-            let y_z = utf8_acc_cont_byte((self.buf[1] & CONT_MASK) as u32, self.buf[2]);
+        let init = utf8_first_byte(x, 2);
+        let y = match self.get_byte() {
+            None => return Some(Codepoint::Invalid((x as u32) << 24)),
+            Some(c) => c,
+        };
+        let mut ch = utf8_acc_cont_byte(init, y);
+        if x >= 0xE0 {
+            let z = match self.get_byte() {
+                None => {
+                    return Some(Codepoint::Invalid(
+                        ((x as u32) << 24) | ((y as u32) << 16)
+                    ))
+                },
+                Some(c) => c,
+            };
+            let y_z = utf8_acc_cont_byte((y & CONT_MASK) as u32, z);
             ch = init << 12 | y_z;
 
-            if self.buf[0] >= 0xF0 {
-               let sz = self.f.read(&mut self.buf[3..]).unwrap();
-                if sz == 0 {
-                    return Some(Codepoint::Invalid(
-                        ((self.buf[0] as u32) << 24) | ((self.buf[1] as u32) << 16)
-                            | ((self.buf[2] as u32) << 8)
-                    ))
-                }
-                ch = (init & 7) << 18 | utf8_acc_cont_byte(y_z, self.buf[3]);
+            if x >= 0xF0 {
+                let w = match self.get_byte() {
+                    None => {
+                        return Some(Codepoint::Invalid(
+                            ((x as u32) << 24) | ((y as u32) << 16) | ((z as u32) << 8)
+                        ))
+                    },
+                    Some(c) => c,
+                };                
+                ch = (init & 7) << 18 | utf8_acc_cont_byte(y_z, w);
             }
-        }
-        self.buf = [0; 4];
+        }        
         Some(Codepoint::Valid(ch))
     }
 }
@@ -90,23 +157,37 @@ struct AtomReader {
     reader: CodepointReader,
 }
 
+impl AtomReader {
+    fn new(path: &Path) -> Self {
+        AtomReader {
+            reader: CodepointReader::new(path)
+        }
+    }
+}
+
 impl Iterator for AtomReader {
     type Item = CharAtom;
 
     fn next(&mut self) -> Option<Self::Item> {
         let raw_codepoint = self.reader.next();
-        // TODO: Handle \r\n
-        // TODO: Unicode validity checks
-        match raw_codepoint.unwrap() {
-            Codepoint::Valid(c) => {
-                match c {
-                    0x0a => Some(CharAtom::Newline(Newline::LF)),
-                    0x0d => Some(CharAtom::Newline(Newline::CR)),
-                    _ => None,
+        let ch = match raw_codepoint {
+            None => return Some(CharAtom::EOF),
+            Some(codepoint) => match codepoint {
+                Codepoint::Invalid(c) => return Some(CharAtom::Invalid(c)),
+                Codepoint::Valid(c) => c,
+            }
+        };
+        return Some(match ch {            
+            0x0a => CharAtom::Newline(Newline::LF),
+            0x0d => {
+                if self.reader.lookahead().unwrap_or(0x00) == 0x0a {
+                    CharAtom::Newline(Newline::CRLF)
+                } else {
+                    CharAtom::Newline(Newline::CR)
                 }
             }
-            _ => None,
-        }
+            _ => CharAtom::Valid(std::char::from_u32(ch).unwrap()),
+        })
     }
 }
 
